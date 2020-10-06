000b60812f0cb3069cadefdc11df52ed61bb2c22
Aayushi Bansal <aayushi1499@gmail.com>
2020-10-06 14:03:39 +0530

    STL

---
+++ stl/containers.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<array>
+using namespace std;
+int main(){
+	array <int,5> data_array = {11,22,77,44};
+	cout<<data_array.at(2);
+	
+}
---
+++ stl/size.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<vector>
+#include <bits/stdc++.h> 
+using namespace std;
+int main(){
+vector<int> array = {[[1,2,3],[5,8,9]]};
+cout<<array.size();
+}
